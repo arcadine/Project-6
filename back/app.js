9ce3a8f76e16c54200520a9c287cd5ed2935c036
arcadine <98621856+arcadine@users.noreply.github.com>
2024-08-20 16:54:28 -0400

    Added multer require line to middleware file

--- back/app.js
+++ back/app.js
@@ -3,7 +3,6 @@ const cors = require('cors');
 const path = require('path');
 const userRoutes = require('./routes/user');
 const sauceRoutes = require('./routes/sauces');
-const multer = require('multer');
 require('./connect');
 
 const app = express();
--- back/jwtMiddleware.js
+++ back/jwtMiddleware.js
@@ -1,5 +1,6 @@
 const jwt = require("jsonwebtoken");
 const { secretKey } = require("./controllers/user");
+const multer = require('multer');
 
 function verifyToken(req, res, next) {
   const token = req.headers.authorization;
